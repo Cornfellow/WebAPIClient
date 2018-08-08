ff21499b6a041a031bf18df355776b0836a4c974
Douglas Smith <31438911+Cornfellow@users.noreply.github.com>
2018-08-08 15:15:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        static void Main(string[] args)
+        {
+            //ProcessRepositories().Wait();
+            var repositories = ProcessRepositories().Result;
+            foreach (var repo in repositories)
+            {   if (repo.Name == "corefx") { 
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.Language);
+                Console.WriteLine();
+                }
+            }
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            Client.DefaultRequestHeaders.Add("User-Agent", ".Net Foundation Repository Reporter");
+            var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+            var streamTask = Client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+            return repositories;
+ 
+        }
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace WebAPIClient
+{
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [DataMember(Name = "homepage")]
+        public Uri Homepage { get; set; }
+
+        [DataMember(Name = "watchers")]
+        public int Watchers { get; set; }
+
+        [DataMember(Name = "language")]
+        public string Language { get; set; }
+
+        [DataMember(Name = "pushed_at")]
+        private string JsonDate { get; set; }
+
+
+
+
+    }
+}
